8f57189fc338e9027f9925891019ab329ff6fece
Egor Moroz <morozil06@mail.ru>
2024-08-13 01:17:12 +0300

    add dohuya vsego

---
+++ mne_pohuy/view.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+def api_endpoint(request):
+    data = {
+        'message': 'Hello from Django!',
+        'status': 'success'
+    }
+    return JsonResponse(data)
