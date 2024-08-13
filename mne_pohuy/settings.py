40e22a58a65b5aea8806887a9b306c9c60364d36
Egor Moroz <morozil06@mail.ru>
2024-08-13 23:25:07 +0300

    add new data base

--- mne_pohuy/settings.py
+++ mne_pohuy/settings.py
@@ -71,10 +71,15 @@ WSGI_APPLICATION = 'mne_pohuy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mne_pohuy',
+        'USER': 'admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
+
 
 
 # Password validation
